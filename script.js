dcd9e3f61cea1b8992055b3c38cfdcbec9942947
Techreborn001 <118982639+Techreborn001@users.noreply.github.com>
2024-01-22 17:21:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*=================typed js======================*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ["funny..","amazing..","cool..","great..","cute..","playful..","sweet..","adorable.."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
